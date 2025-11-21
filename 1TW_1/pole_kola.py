9a7814c410d819270856fdd4dc6c89e770413b0d
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-11-21 12:15:56 +0100

    znaki

--- 1TW_1/pole_kola.py
+++ 1TW_1/pole_kola.py
@@ -1,7 +1,7 @@
 import math
 r=0
 while r <= 0 :
-    r = float(input('podaj promień'))
+    r = float(input('podaj promień='))
 
 pole = math.pi * r**2
 print(f'pole koła o promieniu {r} wynosi {pole }')
