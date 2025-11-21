d464b4d5c6d29fef341f06755723ca9cfb746860
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-11-21 10:00:50 +0100

    znaki

--- 1TZ_1/lancuchy_znakowe.py
+++ 1TZ_1/lancuchy_znakowe.py
@@ -1,8 +1,16 @@
 a = 'dzis jest sroda 2'
  #   0123456789
+b="ala ma psa"
+c='11'
+d="12"
+e=11
+f=12
 print(len(a))
 #Pierwszy znak
 print(a[0])
 #Ostatni znak
 print(a[-1])
-#
+
+print(a+b)  # laczenie znakow
+print(c+d)
+print(e+f)
