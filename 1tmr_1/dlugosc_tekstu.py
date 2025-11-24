b3072d1a4fc4a893f0f5bf1153b8e40d9c7392b8
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-11-24 09:00:41 +0100

    znaki

---
+++ 1tmr_1/dlugosc_tekstu.py
@@ -0,0 +1,5 @@
+def dlugosc_tekstu(text):
+    return len(text)
+
+a = input('Podaj tekst z klawiatury: ')
+print('Dlugosc tekstu ', dlugosc_tekstu(a))
---
+++ 1tmr_1/operacje_na_tekstach_2.py
@@ -0,0 +1,18 @@
+
+a = input('Wprowadź tekst do zmiennej a')
+b = input('Wprowadź tekst do zmiennej b')
+print(f' Drukowanie zmiennej a: {a} ')
+# print(f' Drukowanie zmiennej b: {b} ')
+# # ''' Ile znaków ma tekst w zmiennej a'''
+print('długość napisu a:',len(a))
+# ''' CTRL + /'''
+#
+# ''' Łączenie napisów '''
+c = a + b
+d = b + a
+print('a+b ',c)
+print('b+a ',d)
+# #
+# # ''' Powielanie napisów'''
+print('Powielanie napisów')
+print(20 * 'x-')
