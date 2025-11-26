cd21528cbea3e8c514512914405ef90e921e8a6a
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-11-26 10:43:55 +0100

    znaki

---
+++ 1TW_1/operacje_na_tekstach_1.py
@@ -0,0 +1,20 @@
+
+text = 'napis 2'
+#       0123456
+print(text)
+
+print(text[0]) # pierwszy element
+print(text[-1]) # ostatni element
+# # '''
+
+# # '''
+print(text[1:4]) # drukowanie od elementu drugiego do piątego ale bez piątego
+'''
+# drukowanie od elementu drugiego 
+do piątego ale bez piątego - co drugi element
+'''
+print(text[1:4:2])
+# #
+print('--------------')
+for t in text:
+    print(t)
---
+++ 1TW_1/operacje_na_tkestach_3.py
@@ -0,0 +1,6 @@
+a=input('Napisz jedno zdanie ')
+# print(a[1:])
+print(a.split(' '))
+print(a.split(' ')[0]) # pierwszy element listy
+print(a.split(' ')[-1])
+print(a.split(' ')[::2])
