66c4b0596fc8eff57ab9dd949c1b0c21bd7e93f8
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-11-27 12:35:05 +0100

    znaki

--- 1TZ_1/operacje_na_tkestach_3.py
+++ 1TZ_1/operacje_na_tkestach_3.py
@@ -2,4 +2,4 @@ a=input('Napisz jedno zdanie ')
 print(a.split(' '))
 print(a.split(' ')[0]) # pierwszy element listy
 print(a.split(' ')[-1])
-print(a.split(' ')[::2])
+print(a.split(' ')[::2]) # wypisz co drugi wyraz
