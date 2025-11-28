64d82aac1e3302002918f4153219e246fd16a427
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-11-28 08:28:47 +0100

    znaki

--- 1tmr_1/szyfrowanie_1.py
+++ 1tmr_1/szyfrowanie_1.py
@@ -2,19 +2,19 @@
 
 ''' Kod ASCII  https://pl.wikipedia.org/wiki/ASCII'''
 ''' Zamiana wpisanego znaku na kod ASCII'''
-print(ord('A'))
+# print(ord('@'))
 lista = [1,'s']
-for znak in lista:
-    print(ord(str(znak)))
-#
+# for znak in lista:
+#     print(ord(str(znak)))
+# #
 # #Szyfrowanie
 #
 #
 #
 lista_zaszyfrowana =[]
 
-wyraz = input('Podaj wyraz do zaszyfrowania')
-for znak in wyraz:
+text = input('Podaj tekst do zaszyfrowania')
+for znak in text:
     z = ord(znak)
     lista_zaszyfrowana.append(z)
 print(lista_zaszyfrowana)
