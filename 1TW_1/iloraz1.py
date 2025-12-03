62cee588e728ae175732352a157f7dd236da18dd
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-12-03 11:02:35 +0100

    funkcje

---
+++ 1TW_1/iloraz1.py
@@ -0,0 +1,9 @@
+def iloraz(a,b):
+    return a/b
+
+
+x = float(input('Podaj pierwszą liczbę='))
+y=0
+while y==0:
+    y = float(input('Podaj drugą liczbę='))
+print('iloraz wynosi: ', iloraz(x,y))
