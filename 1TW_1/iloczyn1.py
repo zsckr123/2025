ef3b2fcbd84191eb2b0c3b697f0208241945ef5d
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-12-03 11:19:18 +0100

    funkcje

---
+++ 1TW_1/iloczyn1.py
@@ -0,0 +1,12 @@
+def iloczyn():
+    x=0
+    y=0
+    while x<=0:
+        x = float(input('Podaj pierwszą liczbę='))
+    while y<=0:
+        y = float(input('Podaj drugą liczbę='))
+    return x*y
+
+
+
+print('iloczyn wynosi: ', iloczyn())
