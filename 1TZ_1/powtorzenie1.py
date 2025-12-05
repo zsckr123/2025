cb5f2931f7d1ef2f8b8b833559f0751405d256c8
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2025-12-05 09:57:28 +0100

    funkcje

--- 1TZ_1/powtorzenie1.py
+++ 1TZ_1/powtorzenie1.py
@@ -3,5 +3,6 @@ def laczenie_lancuchow(a,b):
 
 a= input('Podaj pierwszy lancuch=')
 b= input('Podaj drugi lancuch=')
+
 print('polaczony lancuch ',laczenie_lancuchow(a,b)[0])
 print('dlugosc polaczonego lancucha', laczenie_lancuchow(a,b)[1])
