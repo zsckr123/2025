05522ad953a61a12db8a182a92bd9d5275124da9
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-02-18 14:00:46 +0100

    parzystosc liczb

---
+++ 1TZ_1/parzystosc_liczb.py
@@ -0,0 +1,6 @@
+''' Sprawdzanie czy liczba jest parzysta'''
+liczba = int(input('Podaj liczbe'))
+if liczba % 2 ==0:
+    print(f'Liczba {liczba} jest parzysta')
+else:
+    print(f'Liczba {liczba} jest nieparzysta')
