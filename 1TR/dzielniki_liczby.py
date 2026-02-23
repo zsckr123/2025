00172a0869ef4401918d50edf864cd96ac06bcee
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-02-23 09:19:06 +0100

    wlasności liczb

---
+++ 1TR/dzielniki_liczby.py
@@ -0,0 +1,7 @@
+#Dzielniki wlasciwe
+liczba = int(input('Podaj liczbe='))
+lista_dzielnikow = []
+for i in range(2,liczba): # <1,liczba)
+    if liczba % i == 0:
+        lista_dzielnikow.append(i)
+print(f'Lista dzielnikow właściwych liczby {liczba} wynosi {lista_dzielnikow}')
---
+++ 1TR/liczba_pierwsza.py
@@ -0,0 +1,12 @@
+'''Szukanie liczb pierwszych'''
+liczba_pierwsza = int(input('Podaj liczbę do badania='))
+licznik_dzielnikow = 0
+
+
+for i in range(2,int(liczba_pierwsza ** 0.5)+1): # 2**3 oznacza 2^3
+    if liczba_pierwsza % i == 0:
+        licznik_dzielnikow += 1
+if licznik_dzielnikow > 0:
+    print(f'Liczba {liczba_pierwsza} NIE  jest liczbą pierwszą . Liczba dzielnikow {licznik_dzielnikow}')
+else:
+    print(f'Liczba {liczba_pierwsza}  jest liczbą pierwszą . Liczba dzielnikow {licznik_dzielnikow}')
