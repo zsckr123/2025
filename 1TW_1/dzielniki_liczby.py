5a0bee2ecb7360f85c215a966e4d3109b04a1b40
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-02-25 13:40:09 +0100

    liczby zaprzyjaznione

--- 1TW_1/dzielniki_liczby.py
+++ 1TW_1/dzielniki_liczby.py
@@ -1,4 +1,4 @@
-#Dzielniki wlasciwe
+/home/fedora/Pobrane/python/2025/1TW_1/palindrom.py /home/fedora/Pobrane/python/2025/1TW_1/liczby_zaprzyjaznione.py#Dzielniki wlasciwe
 liczba = int(input('Podaj liczbe='))
 lista_dzielnikow = []
 for i in range(2,int(liczba/2)+1): # <1,liczba)
