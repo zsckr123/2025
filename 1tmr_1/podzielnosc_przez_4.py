99079736be80c6ab18d2f96992456a10bb344c3b
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-02-27 12:10:12 +0100

    liczby zaprzyjaznione

--- 1tmr_1/podzielnosc_przez_4.py
+++ 1tmr_1/podzielnosc_przez_4.py
@@ -1,7 +1,11 @@
 liczba = int(input('Podaj liczbe '))
 podziel_4 = str(liczba)
 dlugosc = len(podziel_4)
-print(dlugosc)
+print(f'dlugosc liczby {podziel_4} wynosi {dlugosc}')
 print('ostatnia cyfra ', podziel_4[-1])
 print('przedostatnia cyfra ', podziel_4[-2])
 print('dwie ostatnie cyfry ', int(podziel_4[-2]+podziel_4[-1]) )
+if int(podziel_4[-2]+podziel_4[-1]) % 4 ==0:
+    print(f'{liczba} jest podzielna przez 4')
+else:
+    print(f'{liczba} NIE  jest podzielna przez 4')
