de612310bb7236cb9e76c137872c55b78cec8b7a
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-03-02 08:42:41 +0100

    podzielnosc

---
+++ 1TW_1/podzielnosc_przez_5.py
@@ -0,0 +1,7 @@
+liczba = input('Podaj liczbe ')
+if liczba[-1] =='0' or liczba[-1] =='5' :
+    print(f'{liczba} jest podzielna przez 5')
+else:
+    print(f'{liczba} NIE  jest podzielna przez 5')
+    
+
