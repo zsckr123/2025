c1c0a8344a0e466761f9ac6d250c8dcbb5385c13
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-03-23 08:37:49 +0100

    algorytmy zachlanne

---
+++ 1TW_1/algorytm_zachlanny_wydawanie_reszty.py
@@ -0,0 +1,12 @@
+def reszta(kwota, bankomat):
+    bankomat.sort(reverse=True)
+    wynik = []
+
+    for m in bankomat:
+        while kwota >= m:
+            kwota -= m  
+            wynik.append(m)
+
+    return wynik
+
+print(reszta(2120, [10, 20, 50]))
--- 5tw/losuj_zadanie.py
+++ 5tw/losuj_zadanie.py
@@ -1,2 +1,2 @@
 import random
-print(random.randint(1,11))
+print(random.randint(1,22))
