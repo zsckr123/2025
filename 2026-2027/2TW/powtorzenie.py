994b765c4bdf19a32222d1fb283d3ff3063179cd
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-09-10 09:26:49 +0200

    -

--- 2026-2027/2TW/powtorzenie.py
+++ 2026-2027/2TW/powtorzenie.py
@@ -9,3 +9,13 @@ for i in range(100):
        
     else:
         print(f"LIczba {i} jest nie  parzysta")
+
+''' 3. Suma liczb od 1 do n 
+Za pomocą pętli for oblicz sumę 
+liczb naturalnych 
+od 1 do podanego przez użytkownika n.'''
+n = int(input("podaj ilość liczb"))
+suma = 0
+for i in range(1,n+1):
+    suma+=i
+print(f"Suma {n} liczba wynosi {suma}")
