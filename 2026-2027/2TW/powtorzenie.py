466648dbe40c952407f4e53fad1255eb2a0e0690
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-09-10 09:12:38 +0200

    -

---
+++ 2026-2027/2TW/powtorzenie.py
@@ -0,0 +1,11 @@
+# wyswietelnie 100 liczb parzystych
+for i in range(100):
+    print(2*i)
+# instrukcja if "jesli"
+for i in range(100):
+    
+    if i % 2==0:
+        print(f"LIczba {i} jest parzysta")
+       
+    else:
+        print(f"LIczba {i} jest nie  parzysta")
