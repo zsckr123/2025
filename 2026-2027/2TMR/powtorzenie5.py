853c457894b1d324679355aa303413401be43f2c
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-09-11 10:53:43 +0200

    -

---
+++ 2026-2027/2TMR/powtorzenie5.py
@@ -0,0 +1,4 @@
+# 3n 
+
+for i in range(4,14):
+    print(3*i)
---
+++ 2026-2027/3TW/powtorzenie2.py
@@ -0,0 +1,7 @@
+# instrukcja if "jesli"
+for i in range(100):    
+    if i % 2==0:
+        print(f"LIczba {i} jest parzysta")       
+    else:
+        print(f"LIczba {i} jest nie  parzysta")
+
