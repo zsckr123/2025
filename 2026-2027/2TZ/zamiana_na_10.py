f01a4484883c9de9458efe0cb6b3ef2e2a43ee4b
zsckr123 <miroslaw.butajlo@estudzieniec.pl>
2026-09-21 11:51:32 +0200

    -

--- 2026-2027/2TZ/zamiana_na_10.py
+++ 2026-2027/2TZ/zamiana_na_10.py
@@ -1,6 +1,6 @@
 ''' Zamiana na system dziesiątkowy'''
 # 1100  2^0 2^1 , 2^2, 2^3
-liczba_w_innym_systemie = [1, 1, 1, 1, 0, 1]
+liczba_w_innym_systemie = [1,1,1,1,0,1]
 podstawa = int(input('Podaj podstawę='))
 liczba_10 = 0
 potega = 0
